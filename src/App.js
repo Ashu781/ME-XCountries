9c367bfae68e38b19a5403dc36426695751bfef6
Ashutosh Agrawal <ashu@AshutoshMac.local>
2025-10-25 17:22:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from "react";
+import axios from "axios";
+import Grid from '@mui/material/Grid';
 
 function App() {
+
+  let [countries,setCountries] = useState([]);
+
+   useEffect(()=>{
+
+    async function getData(){
+    try{
+    let response = await axios.get("https://xcountries-backend.labs.crio.do/all");
+    setCountries(response.data);
+    }
+    catch(err){
+    console.error("Error fetching data:",err);
+   }
+   };
+
+    getData();
+   },[]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+   <Grid container spacing={2} sx={{margin:"30px 60px 30px 60px"}}>
+    {countries.map((obj)=>(
+        <Grid size={2} sx={{border:"1px solid black", display:"flex", flexDirection:"column", alignItems:"center", paddingTop:"20px"}}>
+          <img src={obj.flag} alt={obj.name} style={{height: "120px", width: "120px"}}/>
+          <h4 style={{textAlign:"center"}}>{obj.name}</h4>
+        </Grid>
+    )
+    )}
+   </Grid>
+  </>
   );
 }
 
